fdab67194ab0776fc68000e6c05c2289dd23a174
Maksym Maksymov <moamama856@gmail.com>
2025-01-24 12:59:15 +0100

    Fix: Added updated implementation

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  // Создаём копию начального состояния
+  let currentState = { ...state };
+  const stateHistory = [];
+
+  // Обрабатываем каждый экшен
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        break;
+
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach((key) => {
+          delete currentState[key];
+        });
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    // Сохраняем текущую копию состояния в историю
+    stateHistory.push({ ...currentState });
+  });
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
